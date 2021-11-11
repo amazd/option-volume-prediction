181a4c37b7d1a60a99ba5ca464018a427dd75d22
Ling Zhou <lzhou1688@gmail.com>
2021-11-10 23:08:04 -0500

    get_data.py converted from ipynb file. modular + data updating

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,167 @@
+import sys
+import fire
+import questionary
+import pandas as pd
+import numpy as np
+from scipy.stats import ttest_ind
+from pathlib import Path
+
+def get_shift_amount():
+
+    action = questionary.select(
+        "How many days of return should we look forward to?",
+        choices=["3", "4", "5"],
+    ).ask()
+    return action
+
+def intersection (lst1, lst2):
+    return list (set(lst1) & set(lst2))
+
+def union (lst1, lst2):
+    return list (set(lst1) | set(lst2))
+
+def run():
+    """The main function for running the script."""
+
+    call_volume = Path("./data/Call_Volume_20211102.csv")
+    returns = Path("./data/Stock_Returns_20211102.csv")
+    put_volume = Path("./data/Put_Volume_20211102.csv")
+
+    call_volume_df = pd.read_csv(
+        call_volume, 
+        index_col="date", 
+        infer_datetime_format=True, 
+        parse_dates=True
+    )
+
+    price_df = pd.read_csv(
+        returns, 
+        index_col="date", 
+        infer_datetime_format=True, 
+        parse_dates=True
+    )
+
+    put_volume_df = pd.read_csv(
+        put_volume, 
+        index_col="date", 
+        infer_datetime_format=True, 
+        parse_dates=True
+    )
+
+    start_date = "2020-04-01"
+    end_date = "2021-06-30"
+
+    call_volume_df = call_volume_df.loc[start_date:end_date]
+    put_volume_df = put_volume_df.loc[start_date:end_date]
+
+    #nDay Return
+    shift = get_shift_amount()
+    #shift = 3
+
+    # Call/Put Ratio Threshold
+    call_put_ratio_threshold = 2
+
+    sixty_day_rolling_volume_df = call_volume_df.rolling(60).mean()
+
+    price_df.apply(pd.to_numeric)
+
+    shift_amt = shift * -1
+    shifted = price_df.shift(periods=shift_amt, axis="rows")
+    period_day_return = shifted/price_df-1
+
+    ticker_names = list(period_day_return)
+
+    trailing_sixty_day = sixty_day_rolling_volume_df.shift(periods=-1, axis="rows")
+    volume_indicator = 4 * trailing_sixty_day
+
+    good_vol_signal = {}
+    no_vol_signal = {}
+
+    for ticker in ticker_names:
+        i = 0
+        good_vol_signal[ticker] = []
+        no_vol_signal[ticker] = []
+
+        for value in call_volume_df[ticker]:
+            date = call_volume_df.index[i]
+            if(value > volume_indicator[ticker][i]):
+                good_vol_signal[ticker].append(date)
+            else:
+                no_vol_signal[ticker].append(date)
+            i+= 1
+
+
+    call_put_ratio = call_volume_df / put_volume_df
+
+
+    good_ratio_signal = {}
+    no_ratio_signal = {}
+
+    for ticker in ticker_names:
+        i = 0
+        good_ratio_signal[ticker] = []
+        no_ratio_signal[ticker] = []
+
+        for ratio in call_put_ratio[ticker]:
+            date = call_put_ratio.index[i]
+            if(ratio > call_put_ratio_threshold):
+                good_ratio_signal[ticker].append(date)
+            else:
+                no_ratio_signal[ticker].append(date)
+            i+= 1
+
+
+    good_final_signal = {}
+    no_final_signal = {}
+
+    for ticker in ticker_names:
+        good_final_signal[ticker] = intersection(good_vol_signal[ticker], good_ratio_signal[ticker])
+
+    for ticker in ticker_names:
+        no_final_signal[ticker] = union(no_vol_signal[ticker], no_ratio_signal[ticker])
+
+    good_signal_returns = {}
+    no_signal_returns = {}
+
+    for ticker in ticker_names:
+        good_signal_returns[ticker] = []
+        no_signal_returns[ticker] = []
+        for date in good_final_signal[ticker]:
+            change = period_day_return.loc[date][ticker]
+            good_signal_returns[ticker].append(change)
+        for date in no_final_signal[ticker]:
+            change = period_day_return.loc[date][ticker]
+            no_signal_returns[ticker].append(change)
+        
+    total=0
+    for ticker in ticker_names:
+        total = total + len(good_signal_returns[ticker])
+    print(total)
+
+
+    totalno=0
+    for ticker in ticker_names:
+        totalno = totalno + len(no_signal_returns[ticker])
+
+    good_signal_returns_list = []
+    for ticker in ticker_names:
+        good_signal_returns_list = good_signal_returns_list  + good_signal_returns[ticker]
+
+    no_signal_returns_gross_list = []
+    for ticker in ticker_names:
+        no_signal_returns_gross_list = no_signal_returns_gross_list  + no_signal_returns[ticker]
+    no_signal_returns_list = [x for x in no_signal_returns_gross_list if np.isnan(x) == False]
+    len(no_signal_returns_list)
+
+
+
+    print(np.var(good_signal_returns_list))
+    print(np.mean(good_signal_returns_list))
+
+    print(np.var(no_signal_returns_list))
+    print(ttest_ind(a=good_signal_returns_list, b=no_signal_returns_list, equal_var=True))
+
+
+# Entry point for the application. Initiates the run() function.
+if __name__ == "__main__":
+    fire.Fire(run)
---
+++ get_data.py
@@ -0,0 +1,145 @@
+#imports pdblp which allows for directly pulling bloomberg api values into data frames. 
+#pdblp needs to be installed using "pip install pdblp". User needs active bloomberg terminal/api access to use this feature
+import pdblp
+import pandas as pd
+import csv
+from pathlib import Path
+
+def run_all_data():
+    
+    #set the start date and end date
+    start_date='20191101'
+    end_date='20211231'
+
+    data_bdh_list(Path("./data/Tickers.csv"),Path("./data/Stock_Prices_20211102.csv"),start_date, end_date,"PX LAST")
+    data_bdh_list(Path("./data/Tickers.csv"),Path("./data/Call_Volume_20211102.csv"),start_date, end_date,"volume_total_call")
+    data_bdh_list(Path("./data/Tickers.csv"),Path("./data/Put_Volume_20211102.csv"),start_date, end_date,"volume_total_put")
+    data_bdh_list(Path("./data/Tickers.csv"),Path("./data/Market_Cap_20211102.csv"),start_date, end_date,"cur_mkt_cap")
+    data_bdh_list(Path("./data/Tickers.csv"),Path("./data/Open_Interest_20211102.csv"),start_date, end_date,"put_call_open_interest_total")
+    data_bdh_list(Path("./data/Tickers.csv"),Path("./data/Short_Interest_20211102.csv"),start_date, end_date,"si_percent_equity_float")
+    data_bdh_list(Path("./data/Tickers.csv"),Path("./data/Free_Float_20211102.csv"),start_date, end_date,"eqy_free_float_pct")
+    
+    data_bdh_single("SPY",Path("./data/SPY_Price_20211102.csv"),start_date, end_date,"PX LAST")
+    
+    data_bdp_list(Path("./data/Tickers.csv"),Path("./data/Sectors_20211102.csv"),"GICS SECTOR NAME")
+    data_bdp_list(Path("./data/Tickers.csv"),Path("./data/Subsectors_20211102.csv"),"GICS INDUSTRY NAME")
+
+
+    
+def data_bdh_list(tickers_path, save_path, start_date, end_date, bbg_field):
+    
+    #initiate pdblp. debug=True will print out how we are interacting with bloomberg api in case of errors
+    con = pdblp.BCon(debug=False, port=8194, timeout=5000000)    
+    con.start()
+    
+    
+    #pull tickers and store in a list
+    with open(tickers_path, "r") as csvfile:
+        ticker_list = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Read the CSV data
+        for row in csvreader:
+            ticker_list.append(row)
+
+    #ticker_list is a list of lists. Remove the list from list of lists
+    ticker_list = ticker_list[0]
+    
+    #append " US EQUITY" to bloomberg api ticker format. we create a new list that is a copy so that we can rename our headers with the original text format
+    bdh_list = ticker_list.copy()
+
+    for x in range(len(bdh_list)):
+        bdh_list[x] = ticker_list[x] + " US EQUITY"
+    
+    #pull bloomberg data into dataframe
+    data_df=con.bdh(bdh_list, bbg_field, start_date, end_date)
+    
+    #create empty list to populate
+    new_label =[]
+    
+    #loop through and remove " US EQUITY" from each individual column label and add to new_label list
+    for column in data_df:
+        column = column[0].split()[0]
+        new_label.append(column)
+    
+    #rename the column labels without " US EQUITY"
+    data_df.set_axis(new_label,axis=1,inplace=True)
+    
+    #print data to csv
+    data_df.to_csv(save_path)
+
+def data_bdh_single(ticker, save_path, start_date, end_date, bbg_field):
+    
+    #initiate pdblp. debug=True will print out how we are interacting with bloomberg api in case of errors
+    con = pdblp.BCon(debug=False, port=8194, timeout=5000000)    
+    con.start()
+    
+    
+   #place the ticker input into the list to plug bloomberg data pull
+    ticker_list = []
+    ticker_list.append(ticker)
+    
+    #append " US EQUITY" to bloomberg api ticker format. we create a new list that is a copy so that we can rename our headers with the original text format
+    bdh_list = ticker_list.copy()
+
+    for x in range(len(bdh_list)):
+        bdh_list[x] = ticker_list[x] + " US EQUITY"
+    
+    #pull bloomberg data into dataframe
+    data_df=con.bdh(bdh_list, bbg_field, start_date, end_date)
+    
+    #create empty list to populate
+    new_label =[]
+    
+    #loop through and remove " US EQUITY" from each individual column label and add to new_label list
+    for column in data_df:
+        column = column[0].split()[0]
+        new_label.append(column)
+    
+    #rename the column labels without " US EQUITY"
+    data_df.set_axis(new_label,axis=1,inplace=True)
+    
+    #print data to csv
+    data_df.to_csv(save_path)
+    
+def data_bdp_list(tickers_path, save_path, bbg_field):
+    
+    #initiate pdblp. debug=True will print out how we are interacting with bloomberg api in case of errors
+    con = pdblp.BCon(debug=False, port=8194, timeout=5000000)    
+    con.start()
+    
+    #pull tickers and store in a list
+    with open(tickers_path, "r") as csvfile:
+        ticker_list = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Read the CSV data
+        for row in csvreader:
+            ticker_list.append(row)
+
+    #ticker_list is a list of lists. Remove the list from list of lists
+    ticker_list = ticker_list[0]
+    
+    #append " US EQUITY" to bloomberg api ticker format. we create a new list that is a copy so that we can rename our headers with the original text format
+    bdh_list = ticker_list.copy()
+
+    for x in range(len(bdh_list)):
+        bdh_list[x] = ticker_list[x] + " US EQUITY"
+    
+    #pull bloomberg data into dataframe
+    data_df=con.ref(bdh_list, bbg_field)
+    
+    #remove extra data columns
+    data_df=data_df.iloc[:,[0,2]]
+
+    #loop through rows and remove " US EQUITY" from ticker
+    for i in range(len(data_df)):
+        data_df.iloc[i][0] = data_df.iloc[i][0].split()[0]
+   
+    #Transpose and set tickers as column headers
+    #data_df=data_df.transpose()
+    #data_df.columns=data_df.iloc[0]
+    #data_df.drop(data_df.index[0])
+    
+    #print data to csv
+    data_df.to_csv(save_path)

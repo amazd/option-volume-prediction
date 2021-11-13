edd72c4c0bd8bc87af4ecfb5753268831dff2e26
Ahmad Sadraei <sadraei@adobe.com>
2021-11-13 18:02:21 -0500

    updating our app, adding questionary

--- app.py
+++ app.py
@@ -1,32 +1,63 @@
-import sys
 import fire
 import questionary
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-def get_shift_amount():
+ALL_SECTOR_STRING = "All Sectors"
 
-    action = questionary.select(
+def get_user_input():
+
+    # nDay Return
+    shift = questionary.select(
         "How many days of return should we look forward to?",
-        choices=["1", "5", "10"],
+        choices=["5", "10", "1"],
     ).ask()
-    return action
+
+    # Call/Put Ratio Threshold
+    call_put_ratio_threshold = questionary.select(
+        "Which call/put ratio?",
+        choices=["3", "2", "1"],
+    ).ask()
+
+    # Short intereset threshold
+    short_interest_threshold = questionary.select(
+        "Which percentage of short interest threshold?",
+        choices=["5", "10", "20"],
+    ).ask()
+
+    # Multiple of Avg Dailiy Vol
+    adv_multiple = questionary.select(
+        "Which multiple of avg daily volume should we use (?",
+        choices=["4", "3", "2"],
+    ).ask()
+
+    all_sectors_df = pd.read_csv(Path("./data/Sectors_20211102.csv"))
+    all_sector_names = all_sectors_df['value'].dropna().unique().tolist()
+    all_sector_names.insert(0, ALL_SECTOR_STRING)
+
+    filter_input_sector = questionary.select(
+        "Which Sector would you like to filter by?",
+        choices=all_sector_names,
+    ).ask()
+
+    return [int(shift), int(call_put_ratio_threshold), int(short_interest_threshold), int(adv_multiple), filter_input_sector]
 
 def intersection (lst1, lst2):
     return list (set(lst1) & set(lst2))
 
-def union (lst1, lst2):
-    return list (set(lst1) | set(lst2))
-
 def run():
-    """The main function for running the script."""
 
     call_volume = Path("./data/Call_Volume_20211102.csv")
-    returns = Path("./data/Stock_Returns_20211102.csv")
+    prices = Path("./data/Stock_Prices_20211102.csv")
     put_volume = Path("./data/Put_Volume_20211102.csv")
+    SPY_prices = Path("./data/SPY_Price_20211102.csv")
+    short_interest = Path("./data/Short_Interest_20211102.csv")
+    all_sectors = Path("./data/Sectors_20211102.csv")
 
+    # Read in csv files
     call_volume_df = pd.read_csv(
         call_volume, 
         index_col="date", 
@@ -35,7 +66,7 @@ def run():
     )
 
     price_df = pd.read_csv(
-        returns, 
+        prices, 
         index_col="date", 
         infer_datetime_format=True, 
         parse_dates=True
@@ -48,35 +79,71 @@ def run():
         parse_dates=True
     )
 
-    start_date = "2020-04-01"
-    end_date = "2021-06-30"
+    SPY_prices_df = pd.read_csv(
+        SPY_prices, 
+        index_col="date", 
+        infer_datetime_format=True, 
+        parse_dates=True
+    )
 
+    short_interest_df = pd.read_csv(
+        short_interest, 
+        index_col="date", 
+        infer_datetime_format=True, 
+        parse_dates=True
+    )
+
+    all_sectors_df = pd.read_csv(
+        all_sectors
+    )
+
+    # Get user input
+    [shift, call_put_ratio_threshold, short_interest_threshold, adv_multiple, filter_input_sector] = get_user_input()
+
+    print(f"Calculating the rolling avgs...")
+
+    # Calculate the 60d trailing volue and daily percent changes
+    sixty_day_rolling_volume_df = call_volume_df.rolling(60).mean()
+    pct_change_one_day = price_df.pct_change()
+
+    start_date = "2020-04-01"
+    end_date = "2021-11-09"
+
+    #filtering the data down by date range
     call_volume_df = call_volume_df.loc[start_date:end_date]
     put_volume_df = put_volume_df.loc[start_date:end_date]
+    SPY_prices_df = SPY_prices_df.loc[start_date:end_date]
+    price_df = price_df.loc[start_date:end_date]
 
-    #nDay Return
-    shift = get_shift_amount()
-    #shift = 3
+    spy_pct_change = SPY_prices_df.pct_change()
+    spy_roll_var = spy_pct_change.rolling(60).var()
 
-    # Call/Put Ratio Threshold
-    call_put_ratio_threshold = 2
+    #Generating new dataframe for 60d rolling covariance
+    rolling_cov = pct_change_one_day.rolling(60).cov(spy_pct_change['SPY'])
+    rolling_beta = rolling_cov.copy()
 
-    sixty_day_rolling_volume_df = call_volume_df.rolling(60).mean()
+    #Calculating rolling 60d betas for each ticker
+    for ticker in rolling_cov:
+        rolling_beta[ticker] = rolling_cov[ticker]/spy_roll_var['SPY']
 
-    price_df.apply(pd.to_numeric)
-
+    #Incorporating the n-day return shift to calc daily period returns
     shift_amt = shift * -1
     shifted = price_df.shift(periods=shift_amt, axis="rows")
     period_day_return = shifted/price_df-1
 
     ticker_names = list(period_day_return)
 
+    #defining trailing 60d volume df and the volume threshold
     trailing_sixty_day = sixty_day_rolling_volume_df.shift(periods=-1, axis="rows")
-    volume_indicator = 4 * trailing_sixty_day
+    volume_indicator = adv_multiple * trailing_sixty_day
 
+    #starting with empty dictionaries
     good_vol_signal = {}
     no_vol_signal = {}
 
+    print(f"Finding signal based on option volumes...")
+
+    #creating the dictionary with the key being the ticker and the value being a list of triggered dates
     for ticker in ticker_names:
         i = 0
         good_vol_signal[ticker] = []
@@ -90,13 +157,16 @@ def run():
                 no_vol_signal[ticker].append(date)
             i+= 1
 
-
+    #Calculating Call / Put Ratio
     call_put_ratio = call_volume_df / put_volume_df
 
-
+    #Creating empty dictionaries for call put ratio signal
     good_ratio_signal = {}
     no_ratio_signal = {}
 
+    print(f"Finding signal based on call/put ratio...")
+
+    #Creating dictionary for call put ratio signal
     for ticker in ticker_names:
         i = 0
         good_ratio_signal[ticker] = []
@@ -111,56 +181,106 @@ def run():
             i+= 1
 
 
+    #Starting with empty dictionaries
     good_final_signal = {}
     no_final_signal = {}
 
+    #Defining intersection function
+    def intersection (lst1, lst2):
+        return list (set(lst1) & set(lst2))
+
+    #Creating final signal date list within dictionaries
     for ticker in ticker_names:
         good_final_signal[ticker] = intersection(good_vol_signal[ticker], good_ratio_signal[ticker])
 
     for ticker in ticker_names:
-        no_final_signal[ticker] = union(no_vol_signal[ticker], no_ratio_signal[ticker])
+        no_final_signal[ticker] = no_vol_signal[ticker] + no_ratio_signal[ticker]
+
+    #Creating SPY n-day returns
+    spy_shifted = SPY_prices_df.shift(periods=shift_amt, axis="rows")
+    spy_period_day_return = spy_shifted/SPY_prices_df-1
+
+    #Calculating beta-adjusted outperformance as stock return minus beta * SPY return
+
+    beta_adj_outperf = period_day_return.copy()
+    for ticker in ticker_names:
+        beta_adj_outperf[ticker] = period_day_return[ticker] - (spy_period_day_return['SPY'] * rolling_beta[ticker])
 
     good_signal_returns = {}
     no_signal_returns = {}
+
+    print(f"Filtering down signal by beta adjusted outperf (this part will take a min)...")
 
     for ticker in ticker_names:
         good_signal_returns[ticker] = []
         no_signal_returns[ticker] = []
         for date in good_final_signal[ticker]:
-            change = period_day_return.loc[date][ticker]
+            change = beta_adj_outperf.loc[date][ticker]
             good_signal_returns[ticker].append(change)
         for date in no_final_signal[ticker]:
-            change = period_day_return.loc[date][ticker]
+            change = beta_adj_outperf.loc[date][ticker]
             no_signal_returns[ticker].append(change)
-        
+
+    print(f"Preparing results... \n")
+
+    #Calculating number of observations for signal triggered
     total=0
     for ticker in ticker_names:
-        total = total + len(good_signal_returns[ticker])
-    print(total)
-
+        total = total + len(good_signal_returns[ticker]) 
 
     totalno=0
     for ticker in ticker_names:
         totalno = totalno + len(no_signal_returns[ticker])
 
+    #Employing short interest filter
+    short_interest_max  = {}
+    for ticker in short_interest_df:
+        short_interest_max[ticker] = short_interest_df[ticker].max()
+
+    filtered_short_interest = []
+    for ticker in short_interest_max:
+        if(short_interest_max[ticker] > short_interest_threshold):
+            filtered_short_interest.append(ticker)
+
+    #Employing sector selection filter
+    if(filter_input_sector != ALL_SECTOR_STRING):
+        filtered_tickers_df = all_sectors_df[all_sectors_df.value == filter_input_sector]
+        sector_filtered_list = filtered_tickers_df['ticker'].tolist()
+    else:
+        sector_filtered_list = ticker_names
+
+    #Creating final list of returns for statistical analysis
     good_signal_returns_list = []
-    for ticker in ticker_names:
-        good_signal_returns_list = good_signal_returns_list  + good_signal_returns[ticker]
+
+    #Using filters to create list of subset of tickers based on user inputs
+    filtered_ticker_names = intersection(sector_filtered_list, filtered_short_interest)
+
+    for ticker in filtered_ticker_names:
+        good_signal_returns_list = good_signal_returns_list + good_signal_returns[ticker]
 
     no_signal_returns_gross_list = []
-    for ticker in ticker_names:
+    for ticker in filtered_ticker_names:
         no_signal_returns_gross_list = no_signal_returns_gross_list  + no_signal_returns[ticker]
+
+    #filter out the nan
     no_signal_returns_list = [x for x in no_signal_returns_gross_list if np.isnan(x) == False]
-    len(no_signal_returns_list)
+    good_signal_returns_list = [x for x in good_signal_returns_list if np.isnan(x) == False]
 
+    good_signal_returns_list_df = pd.DataFrame(good_signal_returns_list, columns=['Beta-Adjusted Outperformance Returns'])
+    print(good_signal_returns_list_df.describe()) 
+    #good_signal_returns_list_df.hvplot.hist(bins=700, xlim=(-1,2), title="Frequency of Outperformance Returns in Signalled Dates")
 
+    no_signal_returns_list_df = pd.DataFrame(no_signal_returns_list)
+    no_signal_returns_list_df.columns = ["Beta-Adjusted Outperformance Returns (for July 2020 to Nov 2021)"]
+    #no_signal_returns_list_df.hvplot.hist(bins=700, xlim=(-1,2), title="Frequency of Outperformance Returns in Control Dates")
 
-    print(np.var(good_signal_returns_list))
-    print(np.mean(good_signal_returns_list))
+    good_mean = np.mean(good_signal_returns_list)
 
-    print(np.var(no_signal_returns_list))
-    print(ttest_ind(a=good_signal_returns_list, b=no_signal_returns_list, equal_var=True))
-
+    #T-test
+    tt, pvalue = ttest_ind(a=good_signal_returns_list, b=no_signal_returns_list, equal_var=True)
+    print(f"The T-test T-stat is {tt :.2f}")
+    print(f"The T-test p-value is {100*pvalue :.2f}%")
+    print(f"The mean return of our signal is {100*good_mean :.2f}%")
 
 # Entry point for the application. Initiates the run() function.
 if __name__ == "__main__":
